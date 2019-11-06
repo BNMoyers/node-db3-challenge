2437bd529c06554ae4621146d9eaee877aefdbeb
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-06 16:29:35 -0600

    MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,55 +1,51 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile');
+const knex = require("knex");
+const knexConfig = require("../knexfile");
 const db = knex(knexConfig.development);
 
 module.exports = {
-    find,
-    findById,
-    findSteps,
-    add,
-    update, 
-    remove,
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
 };
 
 function find() {
-    return db('schemes');
+  return db("schemes");
 }
 
 function findById(id) {
-    return db('schemes')
+  return db("schemes")
     .where({ id })
     .first();
 }
 
-function findSteps(id){
-    return db('steps')
-    .join(
-        "schemes",
-        "schemes.id",
-        "steps.scheme_id")
+function findSteps(id) {
+  return db("steps")
+    .join("schemes", "schemes.id", "steps.scheme_id")
     .select(
-        "steps.id",
-        "schemes.scheme_name",
-        "steps.step_number", 
-        "steps.instructions"
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
     )
-    .orderBy("steps.step_number")
-    where("schemes.id", id)
+    .orderBy("steps.step_number");
+  where("schemes.id", id);
 }
 
 function add(scheme) {
-    return db('schemes')
-    .insert(scheme)
+  return db("schemes").insert(scheme);
 }
 
 function update(changes, id) {
-    return db('schemes')
+  return db("schemes")
     .update(changes)
-    .where('schemes.id', id)
+    .where("schemes.id", id);
 }
 
 function remove(id) {
-    return db('schemes')
+  return db("schemes")
     .delete()
-    .where('schemes.id', id)
+    .where("schemes.id", id);
 }
